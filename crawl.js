e27534debb7e8f16708d593729fd6692295f5868
vicentefiorito <vicentefiorito@gmail.com>
2024-02-23 19:53:22 -0500

    getURLSFromHTML extracts all the links to websites from an HTML body and converts relative paths to absolute paths

--- crawl.js
+++ crawl.js
@@ -1,3 +1,7 @@
+// imports 
+const jsdom = require('jsdom')
+const {JSDOM} = jsdom
+
 const normalizeURL = (url) => {
     const path = new URL(url)
     let fullPath = `${path.host}${path.pathname}`
@@ -8,11 +12,33 @@ const normalizeURL = (url) => {
     return fullPath
 }
 
-// testing here
-const url1 = normalizeURL('https://blog.boot.dev/path/')
-console.log(url1)
+// function that gets the links to a webpage from markdown
+const getURLSFromHTML = (htmlBody,baseURL) => {
+    // this is going to store all the urls found in the body
+    const urls = []
+    // initializes the object model to the htmlBody parameter
+    const dom = new JSDOM(htmlBody)
+    const aElements = dom.window.document.body.querySelectorAll('a') //this returns an array of all the 'a' tag elements
+    // loops throught all the 'a' elements found within the body
+    for(aElement of aElements){
+        const path = aElement.getAttribute('href')
+        const url = new URL(path,baseURL)
+        urls.push(url.href)
+    }
+    return urls
+}
+
+// some testing here
+const htmlBody = `<html>
+<body>
+    <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+</body>
+</html>`
+
+console.log(getURLSFromHTML(htmlBody))
 
 // exporting the function
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLSFromHTML
 }
