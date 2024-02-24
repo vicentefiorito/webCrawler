1b9c2b4ddc6eb0293853689ef5038046d6d5641b
vicentefiorito <vicentefiorito@gmail.com>
2024-02-23 20:20:05 -0500

    implemented test for getURLSFromHTML functionality

--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const {test,expect} = require('@jest/globals')
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL, getURLSFromHTML} = require('./crawl.js')
 
 // test cases
 test('slash test',() => {
@@ -28,4 +28,36 @@ test('http protocol test',() => {
     const newURL = normalizeURL(url)
     const expectedURL = 'blog.boot.dev/path'
     expect(newURL).toEqual(expectedURL)
+})
+
+test('Absolute URL',() => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody =  '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>'
+    const actualURLS = getURLSFromHTML(inputBody,inputURL)
+    const expectedURLS = ['https://blog.boot.dev/']
+    expect(actualURLS).toEqual(expectedURLS)
+})
+
+test('Relative URL',() => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody =  '<html><body><a href="/path/one"><span>Boot.dev></span></a></body></html>'
+    const actualURLS = getURLSFromHTML(inputBody,inputURL)
+    const expectedURLS = ['https://blog.boot.dev/path/one']
+    expect(actualURLS).toEqual(expectedURLS)
+})
+
+test('Multiple URLS in body',() => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody =  '<html><body><a href="/path/one"><span>Boot.dev></span></a><a href="https://other.com/path/one"><span>Boot.dev></span></a></body></html>'
+    const actualURLS = getURLSFromHTML(inputBody,inputURL)
+    const expectedURLS = ['https://blog.boot.dev/path/one', 'https://other.com/path/one']
+    expect(actualURLS).toEqual(expectedURLS)
+})
+
+test('No URL founded',() => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody =  '<html><body><span>No href here!></span></body></html>'
+    const actualURLS = getURLSFromHTML(inputBody,inputURL)
+    const expectedURLS = []
+    expect(actualURLS).toEqual(expectedURLS)
 })
