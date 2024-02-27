05846161f336731b90dc6b847922d344004ddb20
vicentefiorito <vicentefiorito@gmail.com>
2024-02-27 11:18:48 -0500

    finished implementation of the report section

--- report.js
+++ report.js
@@ -1,5 +1,3 @@
-// imports
-const {crawlPage} = require('./crawl.js')
 
 // this function converts the pages object
 // into something that looks good and can be logged to the function
@@ -10,12 +8,18 @@ const printReport = (pages) => {
 
     // this holds the returned sorted object
     const sortedPages = sortPages(pages)
+    for(const sortedPage of sortedPages) {
+        const url = sortedPage[0]
+        const count = sortedPage[1]
+        console.log(`Found ${count} internal links to ${url}`)
+    }
     
 }
 
 // this function sorts the number of pages in ascending order
 // the pages with the hgihgest number of links are first
 const sortPages = (pages) => {
+    // converts the pages object into a 2D array where each inner array is [link,count]
     const pagesArr = Object.entries(pages)
     pagesArr.sort((pageA,pageB)=> {
         return pageB[1] - pageA[1]
